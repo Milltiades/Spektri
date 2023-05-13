d6510f4331b6ab9f77a3331d01ed6795ddfa5ed3
Milltiades <gigakakulia2017@gmail.com>
2023-05-13 19:17:11 +0400

    add brealpoints for swiper

--- src/components/SwiperComponent.tsx
+++ src/components/SwiperComponent.tsx
@@ -15,11 +15,35 @@ export default function SwiperComponent() {
         delay: 2500,
         disableOnInteraction: false,
       }}
+
       spaceBetween={37}
       slidesPerView={1}
       onSwiper={(swiper: any) => console.log(swiper)}
       onSlideChange={() => console.log("slide change")}
+      breakpoints={{
+        768: {
+          slidesPerView: 2,
+        },
+        1200: {
+          slidesPerView: 3
+        },
+        1600: {
+          slidesPerView: 4
+        }
+      }}
     >
+      <SwiperSlide>
+        <Event />
+        <H1>26th Tbilisi Jazz Festival</H1>
+        <P>Sheraton Grand Tbilisi Metechi Palace</P>
+      </SwiperSlide>
+
+      <SwiperSlide>
+        <Event />
+        <H1>26th Tbilisi Jazz Festival</H1>
+        <P>Sheraton Grand Tbilisi Metechi Palace</P>
+      </SwiperSlide>
+
       <SwiperSlide>
         <Event />
         <H1>26th Tbilisi Jazz Festival</H1>
@@ -52,6 +76,16 @@ const Event = styled.div`
   background: #d9d9d9;
   border-radius: 8px;
   height: 178px;
+  background-image: url('/assets/screen.jpeg');
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+
+  &:hover {
+    box-shadow: rgba(0, 0, 0, 0.8) 5px 5px 55px inset;
+
+  }
+  
 `;
 const H1 = styled.h1`
   font-style: normal;
